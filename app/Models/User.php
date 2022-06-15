4a74bccf58b3287db381ab86e5d8a6eec70865fe
linycken013127 <linycken210127@gmail.com>
2022-06-15 15:16:37 +0800

    Add: Message model, migration, and relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Add the below function
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
