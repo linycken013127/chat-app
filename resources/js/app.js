13e2cff32faff69656992bfa1fe9d95d606c692b
linycken013127 <linycken210127@gmail.com>
2022-06-15 15:37:50 +0800

    Add: ChatsController and View

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,9 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Add these two components.
+Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
+Vue.component('chat-form', require('./components/ChatForm.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +32,30 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    //Store chat messages for display in this array.
+    data: {
+        messages: []
+    },
+    //Upon initialisation, run fetchMessages(). 
+    created() {
+        this.fetchMessages();
+    },
+    methods: {
+        fetchMessages() {
+            //GET request to the messages route in our Laravel server to fetch all the messages
+            axios.get('/messages').then(response => {
+                //Save the response in the messages array to display on the chat view
+                this.messages = response.data;
+            });
+        },
+        //Receives the message that was emitted from the ChatForm Vue component
+        addMessage(message) {
+            //Pushes it to the messages array
+            this.messages.push(message);
+            //POST request to the messages route with the message data in order for our Laravel server to broadcast it.
+            axios.post('/messages', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    }
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/chat', [ChatsController::class, 'index']);
+Route::get('/messages', [ChatsController::class, 'fetchMessages']);
+Route::post('/messages', [ChatsController::class, 'sendMessage']);
